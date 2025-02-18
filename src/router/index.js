1caf8265f82b9dcea3320ae2ab059d7b7ef1dcca
jjq <419875574@qq.com>
2025-02-18 18:14:09 +0800

    build: 更新路由和 Vite 配置
    - 在 router/index.js 中设置 Web History 的 base 路径为 '/MusiMall-FrontEnd-User'
    - 在 vite.config.js 中设置 base 路径为 '/MusiMall-FrontEnd-User/'

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/MusiMall-FrontEnd-User'),
   routes
 })
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MusiMall-FrontEnd-User/',
   plugins: [vue()],
   server: {
     port: 22335
